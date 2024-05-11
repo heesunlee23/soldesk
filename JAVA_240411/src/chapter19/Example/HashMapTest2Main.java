4499cf73d02c2fe5607ac62ecee1723df05f3128
Heesun Lee <132185484+heesunlee23@users.noreply.github.com>
2024-05-11 16:01:32 +0900

    Create HashMapTest2Main.java

---
+++ JAVA_240411/src/chapter19/Example/HashMapTest2Main.java
@@ -0,0 +1,26 @@
+package chapter19.Example;
+
+public class HashMapTest2Main {
+
+	public static void main(String[] args) {
+	
+		HashMapTest2 memberMap = new HashMapTest2();
+		
+		Member memberYim = new Member(1001, "임승연");
+		Member memberKim = new Member(1002, "김연희");
+		Member memberChoi = new Member(1003, "최재호");
+		Member memberNo = new Member(1004, "노승찬");
+		
+		memberMap.addMember(memberYim);
+		memberMap.addMember(memberKim);
+		memberMap.addMember(memberChoi);
+		memberMap.addMember(memberNo);
+		
+		memberMap.showAllMember();
+		
+		memberMap.removeMember(1003);
+		System.out.println("=========");
+		memberMap.showAllMember();
+	}
+
+}
