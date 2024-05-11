08e0ddfc94a22ee16271e2ce6193379e9e19c42b
Heesun Lee <132185484+heesunlee23@users.noreply.github.com>
2024-05-11 16:07:15 +0900

    Update Message.java

--- JAVA_240411/src/chapter19/Example/Message.java
+++ JAVA_240411/src/chapter19/Example/Message.java
@@ -1,37 +1,22 @@
 package chapter19.Example;
 
 public class Message {
-	
+
 	private String command;
 	private String to;
 	
 	public Message(String command, String to) {
-		super();
 		this.command = command;
 		this.to = to;
 	}
+
 	public String getCommand() {
 		return command;
 	}
-	public void setCommand(String command) {
-		this.command = command;
-	}
+
 	public String getTo() {
 		return to;
 	}
-	public void setTo(String to) {
-		this.to = to;
-	}
 	
-	@Override
-	public String toString() {
-		if(command.equals("sendMail")) {
-			return to + "에게" + " 메일을 보냅니다";
-		} else if(command.equals("sendSMS")) {
-			return to + "에게" + " SMS를 보냅니다";
-		} else if(command.equals("sendKakaotalk")) {
-			return to + "에게" + " 카카오톡을 보냅니다";
-		}
-		return command + "" + to;
-	}
+	
 }
